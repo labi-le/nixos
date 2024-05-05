ebdb03bb2edcfedcdffa3360e4a948d103cff6d0
labile <1a6i1e@gmail.com>
2024-05-05 03:33:41 +0300

    fix path

--- system/configuration.nix
+++ system/configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware.nix
+      ./system/hardware.nix
       ./wayland/login-manager.nix
       ./modules/docker.nix
     ];
