4bd1a9c0cfc1a384c25ca9db456b5c0d69c4bbdc
labile <i@labile.cc>
2024-05-06 04:36:43 +0300

    configure zsh

--- system/modules/shell.nix
+++ system/modules/shell.nix
@@ -1,6 +1,23 @@
-{ pkgs }:
+{ pkgs, ... } :
 
 {
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  programs.zsh = {
+    autosuggestions = {
+      enable = true;
+      strategy = [ "completion" ];
+      async = true;
+    };
+    zsh-autoenv.enable = true;
+    syntaxHighlighting.enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "simple";
+      plugins = [ "git" "dotenv" "history" "golang" "z" "github" "yii2" ];
+    };
+
+  };
 }
+
