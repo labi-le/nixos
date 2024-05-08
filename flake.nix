8ad3b9a7f81ac90567715976459367acfbaa4f97
labile <i@labile.cc>
2024-05-08 23:05:27 +0300

    allow unstable pkgs

--- flake.nix
+++ flake.nix
@@ -2,9 +2,8 @@
   description = "My system configuration";
 
   inputs = {
-
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -13,15 +12,21 @@
 
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager, ... }@inputs:
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
 
     let
       system = "x86_64-linux";
+      overlay-unstable = final: prev: {
+        unstable = import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      };
     in {
 
     nixosConfigurations.pc = nixpkgs.lib.nixosSystem {
       specialArgs = {
-        pkgs-stable = import nixpkgs-stable {
+        pkgs-stable = import nixpkgs {
           inherit system;
           config.allowUnfree = true;
         };
@@ -29,12 +34,16 @@
       };
       modules = [
         ./system/configuration.nix
+	({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
       ];
     };
 
     homeConfigurations.labile = home-manager.lib.homeManagerConfiguration {
       pkgs = nixpkgs.legacyPackages.${system};
-      modules = [ ./home-manager/home.nix ];
+      modules = [
+        ./home-manager/home.nix
+	({ config, pkgs, ... }: { nixpkgs.overlays = [ overlay-unstable ]; })
+      ];
     };
   };
 }
