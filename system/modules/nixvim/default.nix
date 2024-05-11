c642d23b7b19083a25c5452be6ff9e9703c79992
labi-le <1a6i1e@gmail.com>
2024-05-11 23:58:29 +0300

    update nixvim

--- system/modules/nixvim/default.nix
+++ system/modules/nixvim/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./common.nix
     ./plugins.nix
+    ./keymaps.nix
   ];
 }
---
+++ system/modules/nixvim/keymaps.nix
@@ -0,0 +1,12 @@
+{
+  programs.nixvim.keymaps = [
+    {
+      key = "ff";
+      action = "<cmd>lua require('telescope.builtin').find_files()<cr>";
+    }
+    {
+      key = "//";
+      action = ":noh<CR>";
+    }
+  ];
+}
