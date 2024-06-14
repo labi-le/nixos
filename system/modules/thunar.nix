e483e985ea723a3904c4fef802d21cd1e61e894c
labi-le <1a6i1e@gmail.com>
2024-06-14 10:47:52 +0300

    fix download dir

--- system/modules/thunar.nix
+++ system/modules/thunar.nix
@@ -4,9 +4,14 @@
   programs.thunar.enable = true;
   programs.xfconf.enable = true;
 
-  programs.thunar.plugins = with pkgs.xfce; [
-    thunar-archive-plugin
-    thunar-volman
+  programs.thunar.plugins = with pkgs; [
+    xfce.thunar-archive-plugin
+    xfce.thunar-volman
+
+  ];
+
+  environment.systemPackages = with pkgs; [
+    xarchiver
   ];
 
   services.gvfs.enable = true;
