3ba7d5afb3f35c41207a941a0ea96affefb80237
labi-le <1a6i1e@gmail.com>
2024-07-21 21:21:31 +0300

    up

--- system/modules/nginx.nix
+++ system/modules/nginx.nix
@@ -21,6 +21,9 @@
       ssl_certificate /etc/ssl/labile.cc.pem;
       ssl_certificate_key /etc/ssl/labile.cc.key;
       ssl_client_certificate /etc/ssl/cloudflare.crt;
+      ssl_stapling off;
+      ssl_verify_client on;
+
     '';
   };
 
