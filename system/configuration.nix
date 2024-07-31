7df54de3f6d8ef91f45067e601102c1e95e76e08
labi-le <1a6i1e@gmail.com>
2024-08-01 00:03:02 +0300

    Squashed commit of the following:
    commit e4b7021c545fb9dfa529dd2526782d38fad368a4
    Author: labi-le <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:57:15 2024 +0300
    
        cs
    
    commit 6f86a0949f0bab8e4b0258a57b0931254c1be90d
    Author: labi-le <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:55:08 2024 +0300
    
        add jq
    
    commit b25b157112e2000687ec76e36661e6cb7917fe9d
    Author: labi-le <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:28:00 2024 +0300
    
        update
    
    commit 53af808453a71bca6b43c0fedaf200df0c94548d
    Author: labi-le <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:22:39 2024 +0300
    
        up state
    
    commit 48e62a19704ea15d7d9b7341ce2aa756b0058b4c
    Author: labi-le <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:22:02 2024 +0300
    
        up state
    
    commit 07fb4d277c7c3796888d6b9708537982d2f589ca
    Author: labile <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:12:17 2024 +0300
    
        cleanup
    
    commit 621e93ce505e5c646901c4da8b5410797ee56ed3
    Author: labile <1a6i1e@gmail.com>
    Date:   Mon Jul 29 07:04:17 2024 +0300
    
        limit boot entries
    
    commit f40d4afbf1613824836841334f354f0c9a591dca
    Author: labile <1a6i1e@gmail.com>
    Date:   Mon Jul 29 06:59:44 2024 +0300
    
        add ctop
    
    commit ddef1ce80a42c32a2008d150bea4b9862da6afd5
    Merge: 1be234f b4bc7dc
    Author: labile <1a6i1e@gmail.com>
    Date:   Mon Jul 29 06:40:19 2024 +0300
    
        Merge branch 'main' of https://github.com/labi-le/nixos
    
    commit 1be234f003e6caddfda18b14d49055ca0b8059ac
    Author: labile <1a6i1e@gmail.com>
    Date:   Mon Jul 29 06:39:59 2024 +0300
    
        add hdparm

--- system/configuration.nix
+++ system/configuration.nix
@@ -6,13 +6,10 @@
       ./modules
     ];
 
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "Europe/Moscow";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -27,7 +24,6 @@
     LC_TIME = "ru_RU.UTF-8";
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
   };
@@ -79,13 +75,7 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
   system.autoUpgrade = {
     enable = false;
     flake = "${config.users.users.labile.home}/nix";
