78cf7030910d8d366e6f2507572e820500f80f56
labi-le <1a6i1e@gmail.com>
2024-08-17 16:30:17 +0300

    fix goland debugger

--- home-manager/modules/ide.nix
+++ home-manager/modules/ide.nix
@@ -1,10 +1,17 @@
-{ pkgs
-, ...
-}:
+{ pkgs, ... }:
 
+let
+  golandCustomDelve = pkgs.jetbrains.goland.overrideAttrs (oldAttrs: {
+    postFixup = (oldAttrs.postFixup or "") + ''
+      rm -f $out/goland/plugins/go-plugin/lib/dlv/linux/dlv
+      ln -s ${pkgs.delve}/bin/dlv $out/goland/plugins/go-plugin/lib/dlv/linux/dlv
+    '';
+
+  });
+in
 {
-  home.packages = with pkgs.jetbrains; [
-    goland
-    phpstorm
+  home.packages = with pkgs; [
+    jetbrains.phpstorm
+    golandCustomDelve
   ];
 }
