a253641d34076f52def6cfd1e5ce02f93ba566bc
labile <1a6i1e@gmail.com>
2024-09-14 20:23:08 +0300

    add nsf

--- system/modules/drive.nix
+++ system/modules/drive.nix
@@ -4,4 +4,17 @@
     fsType = "ext4";
     options = [ "noatime" "nofail" ];
   };
+
+  services.nfs.server = {
+    enable = true;
+    exports = ''
+      /drive/torrents 
+      192.168.1.0/24(rw,sync,no_subtree_check)
+    '';
+  };
+  networking.firewall = {
+    allowedTCPPorts =
+      [ 2049 ];
+    allowedUDPPorts = [ 2049 ];
+  };
 }
