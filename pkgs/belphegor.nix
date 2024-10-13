42a0eba5874c61e3b9a1ce54ca50869ba9990e8f
labi-le <1a6i1e@gmail.com>
2024-10-13 11:13:26 +0300

    up belphegor

--- pkgs/belphegor.nix
+++ pkgs/belphegor.nix
@@ -1,14 +1,14 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  version = "1.1.0";
+  version = "1.1.1";
   name = "belphegor";
 in
 pkgs.stdenv.mkDerivation {
   inherit name version;
   src = pkgs.fetchurl {
     url = "https://github.com/labi-le/belphegor/releases/download/v${version}/belphegor_${version}_linux_amd64";
-    sha256 = "52910b8dfadb30bcf9aad80081a9cf433640c09a0671e9461830a319ab967ea0";
+    sha256 = "52d5b2690b49aa56f0f653447f097b172cac3c91868363c40eea8160cc802651";
   };
 
   dontUnpack = true;
