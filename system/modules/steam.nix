120be746d873a49fa9a9eed65902caf2bfc679fc
labi-le <1a6i1e@gmail.com>
2024-11-10 02:47:35 +0300

    cleanup steam
    up
    
    up
    
    up
    
    up boot

--- system/modules/steam.nix
+++ system/modules/steam.nix
@@ -1,43 +1,8 @@
-{ pkgs, ... }:
+{ ... }:
 {
   programs.steam = {
     enable = true;
-
-    package = with pkgs; steam.override {
-      extraPkgs = pkgs: with pkgs; [
-        jq
-        cabextract
-        wget
-        jdk21
-        ftgl
-        freetype
-        #pkgsi686Linux.libpulseaudio
-        gtk3
-        openjfx
-      ];
-    };
   };
-
-  hardware.graphics.enable32Bit = true;
 
   programs.gamemode.enable = true;
-
-  # Java configuration
-  programs.java = {
-    enable = true;
-    package = pkgs.jdk21;
-  };
-
-  # Environment variables for Java
-  environment.variables = {
-    JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
-    INSTALL4J_JAVA_HOME = "${pkgs.jdk21}/lib/openjdk";
-    PATH = [ "${pkgs.jdk21}/bin" ];
-  };
-
-  environment.systemPackages = with pkgs; [
-    #nix-gaming.faf-client
-    jdk21
-    mpg123
-  ];
 }
