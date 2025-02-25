29a90e95be64ac7c265f1d78db0838c45a7f4c6f
labi-le <1a6i1e@gmail.com>
2025-02-26 01:29:33 +0300

    remove unused

--- hosts/modules/users.nix
+++ hosts/modules/users.nix
@@ -16,11 +16,8 @@
   environment.variables.EDITOR = "nvim";
 
   environment.interactiveShellInit = ''
-    alias ec='nvim ${config.users.users.labile.home}/nix/system/configuration.nix'
     alias n='nvim'
     alias rr='ranger'
-    alias sw='alacritty -e `cd ${config.users.users.labile.home}/nix && make`'
-    alias up='alacritty -e `cd ${config.users.users.labile.home}/nix/ make update`'
     alias ddu='docker update --restart=no $(docker ps -qa)'
     alias dsa='docker stop $(docker ps -qa)'
   '';
