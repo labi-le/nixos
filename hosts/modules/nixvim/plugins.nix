cdda1b6a8e0a6b78d2d33106d627aa7af819d9c5
labi-le <1a6i1e@gmail.com>
2025-02-26 01:45:39 +0300

    add multicursor

--- hosts/modules/nixvim/plugins.nix
+++ hosts/modules/nixvim/plugins.nix
@@ -1,26 +1,26 @@
 { pkgs, ... }:
 
 {
-  programs.nixvim.plugins.transparent.enable = true;
-  # programs.nixvim.plugins.codeium-vim.enable = true;
-  programs.nixvim.plugins.nix.enable = true;
-  programs.nixvim.plugins.auto-save.enable = true;
-  programs.nixvim.plugins.auto-session.enable = true;
-  programs.nixvim.plugins.comment.enable = true;
-  programs.nixvim.plugins.indent-blankline.enable = true;
-  programs.nixvim.plugins.lsp-lines.enable = true;
-  programs.nixvim.plugins = {
-    lsp = {
-      enable = true;
-      servers = {
-        nil_ls.enable = true;
-        gopls.enable = true;
+  programs.nixvim = {
+    extraPlugins = with pkgs; [ vimPlugins.vim-visual-multi ];
+    plugins = {
+      lsp = {
+        enable = true;
+        servers = {
+          nil_ls.enable = true;
+          gopls.enable = true;
+        };
       };
-    };
 
-    lsp-format = {
-      enable = true;
+      lsp-format.enable = true;
+      transparent.enable = true;
+      nix.enable = true;
+      auto-save.enable = true;
+      auto-session.enable = true;
+      comment.enable = true;
+      indent-blankline.enable = true;
+      lsp-lines.enable = true;
+
     };
   };
-
 }
