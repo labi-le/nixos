b6669872135548b739c0f6c65178e8a1d337a921
labi-le <1a6i1e@gmail.com>
2025-03-03 02:15:12 +0300

    refactor waybar

--- home-manager/modules/waybar.nix
+++ home-manager/modules/waybar.nix
@@ -2,145 +2,112 @@
 let
   first = "DP-1";
   second = "DP-2";
+  workspacesConfig = {
+    all-outputs = true;
+    disable-scroll = true;
+    format = "{icon}";
+    format-icons = {
+      "1" = "1";
+      "2" = "2";
+      "3" = "3";
+      "4" = "4";
+      "5" = "5";
+      "6" = "6";
+      "7" = "7";
+      "8" = "8";
+    };
+  };
 in
 {
   programs.waybar = {
     enable = true;
-
-    settings.common.backlight = {
-      format = "{icon} {percent}%";
-      format-icons = [
-        " "
-        " "
-      ];
-      on-scroll-down = "${pkgs.brightnessctl}/bin/brightnessctl -c backlight set 1%-";
-      on-scroll-up = "${pkgs.brightnessctl}/bin/brightnessctl -c backlight set +1%";
-    };
-
-    settings.common.battery = {
-      format = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {capacity}%";
-      format-alt = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {time}";
-      format-charging = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
-      format-critical = "<span size='13000' foreground='#E38C8F'>{icon}</span> {capacity}%";
-      format-full = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
-      format-icons = [
-        " "
-        " "
-        " "
-        " "
-        " "
-      ];
-      format-plugged = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
-      format-warning = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {capacity}%";
-      states = {
-        critical = 15;
-        warning = 30;
-      };
-      tooltip-format = "{time}";
-    };
-
-    settings.common.clock = {
-      format = "{:%a %d %H:%M}";
-      tooltip-format = ''
-        <big>{:%Y %B}</big>
-        <tt><small>{calendar}</small></tt>'';
-    };
-
-    settings.common.cpu = {
-      format = "cpu {usage}%";
-    };
-
-    settings.common.position = "bottom";
-    settings.common.output = second;
-    settings.common.height = 15;
-    settings.common.memory = {
-      format = "mem {}%";
-    };
-    settings.common.disk = {
-      format = "nvme {free}/{total}";
-    };
-    settings.common.modules-center = [ "custom/spotify" ];
-    settings.common.modules-left = [ "sway/workspaces" ];
-    settings.common.modules-right = [
-      "network"
-      "custom/vpn"
-      # "disk"
-      "memory"
-      "cpu"
-      "pulseaudio"
-      "backlight"
-      "sway/language"
-      "battery"
-      "clock"
-    ];
-
-    settings.common.network = {
-      format-disconnected = "";
-      format-linked = "{ifname}";
-      format-wifi = "<span size='13000' foreground='#F2CECF'> </span>{signaldBm}";
-      tooltip-format-wifi = "Signal Strenght: {signalStrength}%";
-      format-ethernet = "{ipaddr}";
-    };
-
-    settings.common.pulseaudio = {
-      format = "<span size='13000' foreground='#EBDDAA'>{icon}</span> {volume}% {format_source}";
-      format-bluetooth = "<span size='13000' foreground='#EBDDAA'>{icon} </span> {volume}%";
-      format-icons = {
-        default = [ " " " " ];
-      };
-      format-muted = "<span size='14000' foreground='#EBDDAA'> </span> Muted";
-
-      on-click = "${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
-      on-click-middle = "${pkgs.pavucontrol}/bin/pavucontrol";
-      on-scroll-up = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +1%";
-      on-scroll-down = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -1%";
-
-      on-click-right = "${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
-      format-source-muted = " ";
-      format-source = "";
-
-      ignored-sinks = [ "Easy Effects Sink" ];
-    };
-
-    settings.second = {
-      height = 10;
-      wlr-workspaces = {
-        all-outputs = true;
-        disable-scroll = true;
-        format = "{icon}";
-        format-icons = {
-          "1" = "1";
-          "2" = "2";
-          "3" = "3";
-          "4" = "4";
-          "5" = "5";
-          "6" = "6";
-          "7" = "7";
-          "8" = "8";
+    settings = [
+      {
+        output = second;
+        height = 15;
+        position = "bottom";
+        modules-left = [ "sway/workspaces" ];
+        modules-center = [ "custom/spotify" ];
+        modules-right = [
+          "network"
+          "custom/vpn"
+          "memory"
+          "cpu"
+          "pulseaudio"
+          "backlight"
+          "sway/language"
+          "battery"
+          "clock"
+        ];
+        "sway/workspaces" = workspacesConfig;
+        backlight = {
+          format = "{icon} {percent}%";
+          format-icons = [ " " " " ];
+          on-scroll-down = "${pkgs.brightnessctl}/bin/brightnessctl -c backlight set 1%-";
+          on-scroll-up = "${pkgs.brightnessctl}/bin/brightnessctl -c backlight set +1%";
         };
-      };
-
-      position = "bottom";
-      output = first;
-      modules-left = [ "sway/workspaces" ];
-    };
-
-    settings.common.wlr-workspaces = {
-      all-outputs = true;
-      disable-scroll = true;
-      format = "{icon}";
-      format-icons = {
-        "1" = "1";
-        "2" = "2";
-        "3" = "3";
-        "4" = "4";
-        "5" = "5";
-        "6" = "6";
-        "7" = "7";
-        "8" = "8";
-      };
-    };
-
+        battery = {
+          format = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {capacity}%";
+          format-alt = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {time}";
+          format-charging = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format-critical = "<span size='13000' foreground='#E38C8F'>{icon}</span> {capacity}%";
+          format-full = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format-icons = [ " " " " " " " " " " ];
+          format-plugged = "<span size='13000' foreground='#B1E3AD'> </span>{capacity}%";
+          format-warning = "<span size='13000' foreground='#B1E3AD'>{icon}</span> {capacity}%";
+          states = {
+            critical = 15;
+            warning = 30;
+          };
+          tooltip-format = "{time}";
+        };
+        clock = {
+          format = "{:%a %d %H:%M}";
+          tooltip-format = ''
+            <big>{:%Y %B}</big>
+            <tt><small>{calendar}</small></tt>'';
+        };
+        cpu = {
+          format = "cpu {usage}%";
+        };
+        memory = {
+          format = "mem {}%";
+        };
+        disk = {
+          format = "nvme {free}/{total}";
+        };
+        network = {
+          format-disconnected = "";
+          format-linked = "{ifname}";
+          format-wifi = "<span size='13000' foreground='#F2CECF'> </span>{signaldBm}";
+          tooltip-format-wifi = "Signal Strenght: {signalStrength}%";
+          format-ethernet = "{ipaddr}";
+        };
+        pulseaudio = {
+          format = "<span size='13000' foreground='#EBDDAA'>{icon}</span> {volume}% {format_source}";
+          format-bluetooth = "<span size='13000' foreground='#EBDDAA'>{icon} </span> {volume}%";
+          format-icons = {
+            default = [ " " " " ];
+          };
+          format-muted = "<span size='14000' foreground='#EBDDAA'> </span> Muted";
+          on-click = "${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
+          on-click-middle = "${pkgs.pavucontrol}/bin/pavucontrol";
+          on-scroll-up = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +1%";
+          on-scroll-down = "${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -1%";
+          on-click-right = "${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+          format-source-muted = " ";
+          format-source = "";
+          ignored-sinks = [ "Easy Effects Sink" ];
+        };
+      }
+      {
+        output = first;
+        height = 10;
+        position = "bottom";
+        modules-left = [ "sway/workspaces" ];
+        "sway/workspaces" = workspacesConfig;
+      }
+    ];
     style = ''
       * {
           border-radius: 0;
@@ -153,13 +120,6 @@ in
           color: #DADAE8;
           background-color: rgba(0, 0, 0, 0.4);
       }
-
-      /* tooltip {
-              border-radius: 15px;
-              border-width: 2px;
-          border-style: solid;
-              border-color: #a4b9ef;
-              } */
 
       #workspaces {
           margin-left: 5px;
@@ -184,9 +144,8 @@ in
       }
 
       #workspaces button:hover {
-          /* background: #332e41; */
           background-color: rgba(0, 0, 0, 0);
-              color: #A4B9EF;
+          color: #A4B9EF;
       }
       
       #memory,
@@ -200,8 +159,6 @@ in
       #mpd,
       #network,
       #language,
-
-
       #network {
           padding-left: 15px;
           padding-right: 2px;
@@ -211,7 +168,6 @@ in
       }
 
       #custom-vpn {
-          /* background: #332E41; */
           padding-right: 10px;
           margin-top: 5px;
           margin-bottom: 5px;
@@ -222,7 +178,6 @@ in
           margin-right: 5px;
           border-radius: 0px 15px 15px 0px;
       }
-
 
       #custom-spotify {
           background: #1DB954;
