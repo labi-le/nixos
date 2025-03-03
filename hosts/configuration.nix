8432f1332386d6155e7b26a160a19ffd8cea2cab
labi-le <1a6i1e@gmail.com>
2025-03-04 02:41:05 +0300

    update notebook conf
    add binary cache for all
    
    add binary cache for all
    
    add binary cache for all

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -40,14 +40,4 @@
   packages = { desktop = true; dev = true; };
 
   networking.interfaces.enp37s0.wakeOnLan.enable = true;
-
-  nix.settings = {
-    substituters = [ "https://cache.nixos.org/" "https://cosmic.cachix.org/" "https://cache.garnix.io" ];
-    trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-    auto-optimise-store = true;
-  };
 }
