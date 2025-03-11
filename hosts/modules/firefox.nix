8d70aca0306c438a10699f80ff100ca102f1fec6
labi-le <1a6i1e@gmail.com>
2025-03-11 22:33:14 +0300

    enable firefox

--- hosts/modules/firefox.nix
+++ hosts/modules/firefox.nix
@@ -21,6 +21,34 @@
       "privacy.trackingprotection.fingerprinting.enabled" = true;
       "privacy.trackingprotection.socialtracking.enabled" = true;
       "privacy.trackingprotection.socialtracking.annotate.enabled" = true;
+      "extensions.fxmonitor.enabled" = false;
+    };
+    policies = {
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+
+      # nix run github:tupakkatapa/mozid -- ''
+      ExtensionSettings = with builtins;
+        let
+          extension = shortId: uuid: {
+            name = uuid;
+            value = {
+              install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+              installation_mode = "normal_installed";
+            };
+          };
+        in
+        listToAttrs [
+          (extension "uBlock Origin" "uBlock0@raymondhill.net")
+          (extension "Bitwarden Password Manager" "{446900e4-71c2-419f-a6a7-df9c091e268b}")
+          (extension "ClearURLs" "{74145f27-f039-47ce-a470-a662b129930a}")
+          (extension "Return YouTube Dislike" "{762f9885-5a13-4abd-9c77-433dcd38b8fd}")
+          (extension "Buster: Captcha Solver for Humans" "{e58d3966-3d76-4cd9-8552-1582fbc800c1}")
+          (extension "DeepL" "{ea41402b-23b4-4ad1-97fb-7b6396789243}")
+
+        ];
     };
   };
+
 }
