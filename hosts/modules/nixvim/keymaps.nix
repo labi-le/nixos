82f0959caae8b2bc53ec180f3a1629dfeeb8630b
labi-le <1a6i1e@gmail.com>
2025-04-16 05:39:44 +0300

    add new hotkeys nixvim

--- hosts/modules/nixvim/keymaps.nix
+++ hosts/modules/nixvim/keymaps.nix
@@ -20,5 +20,52 @@
         "v"
       ];
     }
+    {
+      key = "J";
+      action = "]]";
+      mode = [
+        "n"
+        "v"
+      ];
+      options = {
+        desc = "next code block (TreeSitter)";
+      };
+    }
+
+    {
+      key = "K";
+      action = "[[";
+      mode = [
+        "n"
+        "v"
+      ];
+      options = {
+        desc = "previous code block (TreeSitter)";
+      };
+    }
+
+    {
+      key = "j";
+      action = "]m";
+      mode = [
+        "n"
+        "v"
+      ];
+      options = {
+        desc = "next fn (TreeSitter)";
+      };
+    }
+
+    {
+      key = "k";
+      action = "[m";
+      mode = [
+        "n"
+        "v"
+      ];
+      options = {
+        desc = "previous fn (TreeSitter)";
+      };
+    }
   ];
 }
