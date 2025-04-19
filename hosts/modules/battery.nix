090ffefca345e3937e97b3dd34ee062a874ef041
labi-le <1a6i1e@gmail.com>
2025-04-19 19:31:44 +0300

    auto-cpufreq

--- hosts/modules/battery.nix
+++ hosts/modules/battery.nix
@@ -1,20 +1,37 @@
 { pkgs, lib, ... }:
 {
-  services.tlp = {
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+  #     CPU_MIN_PERF_ON_AC = 0;
+  #     CPU_MAX_PERF_ON_AC = 100;
+  #     CPU_MIN_PERF_ON_BAT = 0;
+  #     CPU_MAX_PERF_ON_BAT = 70;
+  #     START_CHARGE_THRESH_BAT0 = 40;
+  #     STOP_CHARGE_THRESH_BAT0 = 80;
+  #   };
+  # };
+
+  services.auto-cpufreq = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-      START_CHARGE_THRESH_BAT0 = 40;
-      STOP_CHARGE_THRESH_BAT0 = 80;
+      battery = {
+        governor = "schedutil";
+        turbo = "never";
+        platform_profile = "low-power";
+        energy_performance_preference = "balance_power";
+      };
+      charger = {
+        governor = "performance";
+        turbo = "auto";
+      };
     };
   };
+
   powerManagement.enable = true;
   zramSwap = {
     enable = true;
