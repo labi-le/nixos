a12252ea7bc95f1932af4fc03b63c5851a787d70
labi-le <1a6i1e@gmail.com>
2025-04-21 17:46:23 +0300

    update

--- hosts/configuration-server.nix
+++ hosts/configuration-server.nix
@@ -30,25 +30,13 @@
     "w /sys/class/power_supply/BAT0/charge_control_end_threshold - - - - 60"
   ];
 
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
-
   hardware.graphics.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
   programs.gnupg.agent = {
     enable = true;
   };
 
   system.stateVersion = "24.11";
-
-  nix.gc.automatic = true;
-  nix.gc.dates = "daily";
 
   packages = {
     server = true;
