7306a46b9c8b4dab3be359850b933c0cd5231fee
labi-le <1a6i1e@gmail.com>
2025-07-01 12:09:00 +0300

    remove sshfs

--- settings.nix
+++ settings.nix
@@ -14,12 +14,12 @@
       substituters = [
         "https://cache.nixos.org?priority=-1"
         "https://cosmic.cachix.org?priority=10"
-        "https://cache.garnix.io?priority=100"
+        # "https://cache.garnix.io?priority=100"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        # "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       ];
       trusted-users = [ "@wheel" ];
       experimental-features = [
