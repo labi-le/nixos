2e5505bbc60100e5d6a1793033b16502f088c872
labi-le <1a6i1e@gmail.com>
2025-07-01 13:04:55 +0300

    fix drive mount

--- modules/drive.nix
+++ modules/drive.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   fileSystems."/drive" = {
     device = "/dev/disk/by-uuid/b631c90c-690f-4cf0-9775-56c53f69f5b5";
@@ -12,18 +14,19 @@
     description = "Set readahead for /dev/sda";
     wantedBy = [ "local-fs.target" ];
     serviceConfig.Type = "oneshot";
-    serviceConfig.ExecStart = "/usr/bin/blockdev --setra 1024 /dev/sda";
+    serviceConfig.ExecStart = "${pkgs.util-linux}/bin/blockdev --setra 1024 /dev/sda";
   };
 
   services.nfs.server = {
     enable = true;
     exports = ''
       /drive/
-      192.168.1.0/24(rw,sync,no_subtree_check)
+      192.168.1.0/24(rw,sync,no_subtree_check,root_squash)
     '';
+    nproc = 16;
+
   };
   networking.firewall = {
     allowedTCPPorts = [ 2049 ];
-    allowedUDPPorts = [ 2049 ];
   };
 }
