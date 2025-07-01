7e636220aaf9fc806b65810aa03fd92fa175fb58
labi-le <1a6i1e@gmail.com>
2025-07-01 12:09:00 +0300

    remove sshfs

--- modules/sshfs.nix
+++ modules/sshfs.nix
@@ -1,15 +1,30 @@
 {
   # sudo ssh-copy-id labile@192.168.1.2
+  # fileSystems."/home/drive" = {
+  #   device = "labile@192.168.1.2:/drive";
+  #   fsType = "sshfs";
+  #   options = [
+  #     "allow_other"
+  #     "_netdev"
+  #     "x-systemd.automount"
+  #     # SSH options
+  #     "reconnect" # handle connection drops
+  #     "ServerAliveInterval=15" # keep connections alive
+  #   ];
+  # };
+
   fileSystems."/home/drive" = {
-    device = "labile@192.168.1.2:/drive";
-    fsType = "sshfs";
+    device = "192.168.1.2:/drive";
+    fsType = "nfs";
     options = [
-      "allow_other"
-      "_netdev"
-      "x-systemd.automount"
-      # SSH options
-      "reconnect" # handle connection drops
-      "ServerAliveInterval=15" # keep connections alive
+      "auto"
+      "nofail"
+      "noatime"
+      "nolock"
+      "intr"
+      "tcp"
+      "actimeo=1800"
     ];
   };
+
 }
