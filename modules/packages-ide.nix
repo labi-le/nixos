f6156056cfb451789bfd5bf4e491c271492c00c2
labi-le <1a6i1e@gmail.com>
2025-07-17 10:44:14 +0300

    remove kostyl`

--- modules/packages-ide.nix
+++ modules/packages-ide.nix
@@ -7,12 +7,5 @@ with pkgs;
   jetbrains.phpstorm
   # jetbrains.clion
   # jetbrains.pycharm-professional
-  (jetbrains.goland.overrideAttrs (oldAttrs: {
-    postFixup =
-      (oldAttrs.postFixup or "")
-      + ''
-        rm -f $out/goland/plugins/go-plugin/lib/dlv/linux/dlv
-        ln -s ${delve}/bin/dlv $out/goland/plugins/go-plugin/lib/dlv/linux/dlv
-      '';
-  }))
+  jetbrains.goland
 ]
