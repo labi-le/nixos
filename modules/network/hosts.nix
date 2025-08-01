463747248258b937a35eebec79de46f898fa7fa2
labi-le <1a6i1e@gmail.com>
2025-08-01 18:39:35 +0300

    fix resolve local gomains

--- modules/network/hosts.nix
+++ modules/network/hosts.nix
@@ -2,4 +2,13 @@
   networking.hosts = {
     "172.16.0.11" = [ "forms.local.ru" ];
   };
+
+  services.resolved = {
+    enable = true;
+    extraConfig = ''
+      [Resolve]
+      DNS=192.168.1.1
+      Domains=~passport.local
+    '';
+  };
 }
