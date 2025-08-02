9c90df22af6f6edf799a2504892de968af6d64de
labi-le <1a6i1e@gmail.com>
2025-08-02 17:51:32 +0300

    add fail2ban

---
+++ modules/fail2ban.nix
@@ -0,0 +1,21 @@
+{ ... }:
+{
+  services.fail2ban = {
+    enable = true;
+    maxretry = 5;
+    ignoreIP = [
+      "10.0.0.0/8"
+      "172.16.0.0/12"
+      "192.168.0.0/16"
+    ];
+    bantime = "24h";
+    bantime-increment = {
+      enable = true;
+      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "168h";
+      overalljails = true;
+    };
+    jails = { };
+  };
+}
