7a34cd26364db9a82aa7389a4dd004335dc617ac
labi-le <1a6i1e@gmail.com>
2025-08-17 02:24:45 +0300

    disable systemd-resolved

--- modules/network/hosts.nix
+++ modules/network/hosts.nix
@@ -3,12 +3,18 @@
     "172.16.0.11" = [ "forms.local.ru" ];
   };
 
-  services.resolved = {
-    enable = true;
-    extraConfig = ''
-      [Resolve]
-      DNS=192.168.1.1
-      Domains=~passport.local
-    '';
-  };
+  # services.resolved = {
+  #   enable = true;
+  #   extraConfig = ''
+  #     [Resolve]
+  #     DNS=192.168.1.1
+  #     Domains=~passport.local
+  #   '';
+  # };
+
+  services.resolved.enable = false;
+  networking.networkmanager.dns = "default";
+  environment.etc."resolv.conf".text = ''
+    nameserver 192.168.1.1
+  '';
 }
