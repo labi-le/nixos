683bc8dd4d82f6023f266ef4396a6a3717cc504f
labi-le <1a6i1e@gmail.com>
2025-08-20 02:49:34 +0300

    fix geyrover

--- modules/packages-ide.nix
+++ modules/packages-ide.nix
@@ -1,9 +1,24 @@
 { pkgs }:
-
 with pkgs;
 [
   postman
-  jetbrains.rust-rover
+  (symlinkJoin {
+    name = "rust-rover-wrapped";
+    paths = [ jetbrains.rust-rover ];
+    buildInputs = [ makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/rust-rover \
+        --set PKG_CONFIG_PATH "${openssl.dev}/lib/pkgconfig" \
+        --set LD_LIBRARY_PATH "${lib.makeLibraryPath [ openssl ]}" \
+        --prefix PATH : "${
+          lib.makeBinPath [
+            pkg-config
+            cargo
+            rustup
+          ]
+        }"
+    '';
+  })
   jetbrains.phpstorm
   # jetbrains.clion
   # jetbrains.pycharm-professional
