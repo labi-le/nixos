1b2b5dbd437e262d7b5a70e1df2b22c146f927d4
labi-le <1a6i1e@gmail.com>
2025-08-24 02:13:07 +0300

    test kernel for notebook

--- modules/kernel.nix
+++ modules/kernel.nix
@@ -1,12 +1,29 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   boot.kernelParams = [
     "nvme_core.default_ps_max_latency_us=0"
     "mitigations=off"
+    "amd_pstate=passive"
+
   ];
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.blacklistedKernelModules = [
     "snd_pcsp"
     "pcspkr"
+  ];
+
+  boot.kernelPatches = [
+    {
+      name = "ryzen-4500u-optimization";
+      patch = null;
+
+      structuredExtraConfig = with lib.kernel; {
+        NO_HZ_IDLE = yes;
+        HZ_250 = yes;
+        X86_AMD_PSTATE = yes;
+        NO_HZ_FULL = lib.mkForce no;
+
+      };
+    }
   ];
 }
