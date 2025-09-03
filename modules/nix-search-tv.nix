01f57cdf3849459c36927da9c4fdd52d370956ff
labi-le <1a6i1e@gmail.com>
2025-09-03 19:12:26 +0300

    wrap ns

--- modules/nix-search-tv.nix
+++ modules/nix-search-tv.nix
@@ -1,9 +1,17 @@
 { pkgs, ... }:
 {
+  environment.systemPackages = with pkgs; [
+    (buildEnv {
+      name = "nix-search-tv-with-fzf";
+      paths = [
+        nix-search-tv
+        fzf
+      ];
+      pathsToLink = [ "/bin" ];
+    })
+  ];
 
-  environment.systemPackages = with pkgs; [ nix-search-tv ];
   environment.interactiveShellInit = ''
     alias ns="nix-search-tv print | fzf --preview 'nix-search-tv preview {}' --scheme history"
   '';
-
 }
