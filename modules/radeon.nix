667882490a9f02ac650b53f618c3cfd2429fd3d9
labi-le <1a6i1e@gmail.com>
2025-10-19 23:02:33 +0300

    upgrade

--- modules/radeon.nix
+++ modules/radeon.nix
@@ -7,25 +7,8 @@
     graphics = {
       enable = true;
       extraPackages = with pkgs; [
-        amdvlk
       ];
     };
-
-    amdgpu = {
-      amdvlk.enable = true;
-    };
   };
-  environment.systemPackages = with pkgs; [
-    lact
-  ];
-
-  systemd.services.lact = {
-    description = "AMDGPU Control Daemon";
-    after = [ "multi-user.target" ];
-    wantedBy = [ "multi-user.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.lact}/bin/lact daemon";
-    };
-    enable = true;
-  };
+  services.lact.enable = true;
 }
