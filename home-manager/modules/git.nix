53c56b531f442b067815a54c58af288a2d494411
labi-le <1a6i1e@gmail.com>
2025-10-19 23:02:33 +0300

    upgrade

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -3,13 +3,23 @@
 {
   programs.git = {
     enable = true;
-    userName = "labi-le";
-    userEmail = "1a6i1e@gmail.com";
-    extraConfig = {
-      push = { autoSetupRemote = true; };
-      color.ui = true;
-      core.editor = "nvim";
-      credential.helper = "store";
+    settings = {
+      user = {
+        name = "labi-le";
+        email = "1a6i1e@gmail.com";
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+      color = {
+        ui = true;
+      };
+      core = {
+        editor = "nvim";
+      };
+      credential = {
+        helper = "store";
+      };
     };
   };
 }
