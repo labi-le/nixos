83fffb7f882ab9be0144b754d4b2164474271cbb
labi-le <1a6i1e@gmail.com>
2025-11-14 00:04:15 +0300

    fix ssh perm

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,8 +1,16 @@
 { ... }:
 {
   programs.gnupg.agent.enableSSHSupport = true;
-  services.openssh = { enable = true; };
+  services.openssh = {
+    enable = true;
+  };
+
+  networking.firewall = {
+    extraCommands = ''
+      iptables -I nixos-fw -p tcp --dport 22 -s 192.168.0.0/16 -j nixos-fw-accept
+    '';
+  };
+
   security.pam.services.sshd.showMotd = true;
   security.pam.sshAgentAuth.enable = true;
 }
-
