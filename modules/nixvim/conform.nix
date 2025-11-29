61558925debb92a5c04255ecc1bf9cf9cad009b2
labi-le <1a6i1e@gmail.com>
2025-11-29 20:49:22 +0300

    add sh syntaxhighlight

--- modules/nixvim/conform.nix
+++ modules/nixvim/conform.nix
@@ -1,12 +1,19 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
 
   programs.nixvim = {
     plugins = {
       conform-nvim = {
         enable = true;
         settings = {
-          format_on_save = { lsp_format = "fallback"; };
-          formatters_by_ft = { php = [ "php_cs_fixer" ]; };
+          format_on_save = {
+            lsp_format = "fallback";
+          };
+          formatters_by_ft = {
+            php = [ "php_cs_fixer" ];
+            sh = [ "shfmt" ];
+            bash = [ "shfmt" ];
+          };
           formatters = {
             # phpcbf = {
             #   command = "${pkgs.php83Packages.php-codesniffer}/bin/phpcbf";
@@ -14,7 +21,24 @@
             # };
             php_cs_fixer = {
               command = "${pkgs.php83Packages.php-cs-fixer}/bin/php-cs-fixer";
-              args = [ "fix" "$FILENAME" "--rules" "@PER-CS2.0" ];
+              args = [
+                "fix"
+                "$FILENAME"
+                "--rules"
+                "@PER-CS2.0"
+              ];
+            };
+            shfmt = {
+              command = "${pkgs.shfmt}/bin/shfmt";
+              args = [
+                "-i"
+                "2"
+                "-bn"
+                "-ci"
+                "-sr"
+                "-filename"
+                "$FILENAME"
+              ];
             };
           };
         };
--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -17,51 +17,14 @@
       dap = {
         enable = true;
         configurations = {
-          # go = [
-          #   {
-          #     name = "Launch file";
-          #     type = "go";
-          #     request = "launch";
-          #     program = "\${file}";
-          #   }
-          #   {
-          #     name = "Debug test";
-          #     type = "go";
-          #     request = "launch";
-          #     mode = "test";
-          #     program = "\${file}";
-          #   }
-          #   # Можно добавить конфигурацию для подключения к удаленному процессу delve
-          #   # {
-          #   #   name = "Attach remote";
-          #   #   type = "go";
-          #   #   request = "attach";
-          #   #   mode = "remote";
-          #   #   port = 2345; # Укажите нужный порт
-          #   #   host = "127.0.0.1";
-          #   # }
-          # ];
           php = [
             {
               name = "Listen for Xdebug";
               type = "php";
               request = "launch";
               port = 9003;
-              # pathMappings может потребоваться, если ваш код PHP работает в Docker или VM
-              # pathMappings = {
-              #   "/path/inside/container" = "\${workspaceFolder}";
-              # };
-              log = false; # Установите в true для логов отладки адаптера
+              log = false;
             }
-            # Вы можете добавить конфигурацию для запуска текущего скрипта PHP
-            # {
-            #   name = "Launch current script";
-            #   type = "php";
-            #   request = "launch";
-            #   program = "\${file}";
-            #   cwd = "\${workspaceFolder}";
-            #   runtimeExecutable = "php"; # Или укажите путь к php
-            # }
           ];
         };
       };
@@ -132,6 +95,14 @@
 
     extraConfigLua = ''
       vim.g.transparent_enabled = true
+
+      vim.api.nvim_create_autocmd("CursorHold", {
+        callback = function()
+          vim.diagnostic.open_float(nil, { focus = false })
+        end
+      })
+
+      vim.opt.updatetime = 500
     '';
   };
 
