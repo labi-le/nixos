6fb2e751ff1e78e4531c62f7d31355812991e425
labi-le <1a6i1e@gmail.com>
2025-12-25 18:53:04 +0300

    update nvim

--- modules/nixvim/dap.nix
+++ modules/nixvim/dap.nix
@@ -35,61 +35,75 @@
     ];
 
     extraConfigLua = ''
-           local dap, dapui = require("dap"), require("dapui")
-           dap.listeners.before.attach.dapui_config = function()
-             dapui.open()
-           end
+      local dap, dapui = require("dap"), require("dapui")
 
-           dap.listeners.before.launch.dapui_config = function()
-           dapui.open()
-           end
+      dapui.setup({
+        layouts = {
+          {
+            elements = {
+              { id = "console" },
+            },
+            size = 2,
+            position = "top",
+          },
+          {
+            elements = {
+              { id = "scopes" },
+            },
+            size = 15,
+            position = "bottom",
+          },
+          {
+            elements = {
+              { id = "watches", size = 0.33 },
+              { id = "repl", size = 0.33 },
+              { id = "stacks", size = 0.34 },
+            },
+            size = 30,
+            position = "left",
+          },
+        },
+      })
 
-           dap.listeners.before.event_terminated.dapui_config = function()
-             dapui.close()
-           end
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
 
-           dap.listeners.before.event_exited.dapui_config = function()
-             dapui.close()
-           end
+      dap.listeners.before.launch.dapui_config = function()
+      dapui.open()
+      end
 
-           local hint = [[
-          ^ ^Step^ ^ ^      ^ ^     Action
-      ----^-^-^-^--^-^----  ^-^-------------------
-          ^ ^back^ ^ ^     ^_t_: toggle breakpoint
-          ^ ^ _p_^ ^        _T_: clear breakpoints
-      out _N_ ^ ^_<CR>_into _x_: terminate
-          ^ ^ _n_ ^ ^       ^^_r_: open repl
-          ^ ^over ^ ^
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
 
-          ^ ^  _q_: exit
-           ]]
-           vim.api.nvim_set_hl(0, "NormalFloat", {fg="NONE", bg="NONE"})
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
 
-           require('hydra') {
-             name = 'Debug',
-             --hint = hint,
-             config = {
-               color = 'pink',
-               invoke_on_body = true,
-               --hint = {
-               --  type = 'window'
-               --},
-             },
-             mode = { 'n' },
-             body = '<leader>d',
-             heads = {
-               { 'p', dap.step_back, { desc = 'prev' } },
-               { '<CR>', dap.step_into, { desc = 'into' } },
-               { 'N', dap.step_out, { desc = 'out' } },
-               { 'n', dap.step_over, { desc = 'over' } },
-               { 't', dap.toggle_breakpoint, { desc = 'toggle breakpoint' } },
-               { 'T', dap.clear_breakpoints, { desc = 'clear breakpoints' } },
-               { 'x', dap.terminate, { desc = 'terminate' } },
-               { 'r', dap.repl.open, { exit = true, desc = 'open repl' } },
-               { 'q', nil, { exit = true, nowait = true, desc = 'exit' } },
-               { 'c', dap.continue, { desc = 'continue' } },
-             }
-           }
+      vim.api.nvim_set_hl(0, "NormalFloat", {fg="NONE", bg="NONE"})
+
+      require('hydra') {
+        name = 'Debug',
+        config = {
+          color = 'pink',
+          invoke_on_body = true,
+        },
+        mode = { 'n' },
+        body = '<leader>d',
+        heads = {
+          { 'p', dap.step_back, { desc = 'prev' } },
+          { '<CR>', dap.step_into, { desc = 'into' } },
+          { 'N', dap.step_out, { desc = 'out' } },
+          { 'n', dap.step_over, { desc = 'over' } },
+          { 't', dap.toggle_breakpoint, { desc = 'toggle breakpoint' } },
+          { 'T', dap.clear_breakpoints, { desc = 'clear breakpoints' } },
+          { 'x', dap.terminate, { desc = 'terminate' } },
+          { 'r', dap.repl.open, { exit = true, desc = 'open repl' } },
+          { 'q', nil, { exit = true, nowait = true, desc = 'exit' } },
+          { 'c', dap.continue, { desc = 'continue' } },
+        }
+      }
     '';
     globals.mapleader = " ";
 
--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -11,7 +11,6 @@
   ];
   programs.nixvim = {
     plugins = {
-      dap-ui.enable = true;
       dap-go.enable = true;
       cmp-dap.enable = true;
       dap = {
