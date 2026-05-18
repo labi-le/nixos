b89408c4727a373187212a807af3408792b5ff0b
labi-le <1a6i1e@gmail.com>
2026-05-18 16:16:57 +0300

    add ds2 fix

--- modules/gamepad.nix
+++ modules/gamepad.nix
@@ -1,4 +1,9 @@
 { ... }:
 {
   hardware.xpadneo.enable = true;
+
+  # https://steamcommunity.com/sharedfiles/filedetails/?id=3608372415
+  services.udev.extraRules = ''
+    KERNEL=="event*", SUBSYSTEM=="input", ENV{ID_VENDOR_ID}=="3434", ENV{ID_INPUT_JOYSTICK}=="*?", ENV{ID_INPUT_JOYSTICK}=""
+  '';
 }
