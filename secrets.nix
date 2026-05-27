f31d3f42aca05dda37511cef1e6f44564d847c8c
labi-le <1a6i1e@gmail.com>
2026-05-28 00:50:49 +0300

    fix pubkey

--- secrets.nix
+++ secrets.nix
@@ -1,5 +1,5 @@
 let
-  notebook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOAKcBEt8dosLBD24bRIlIHEArLwbxmGEjehZCSRV4w1 labile@notebook";
+  notebook = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPozMe3Gt6UIj00f/PD/vpWGnJvoF1N77GoQjeyBvZCE root@notebook";
   pc = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmj4DhtDs5bZhqTK6NiolqRgNCnGWyxty4LRixuU77Z labile@pc";
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrOmpc2RqBzvSgNAIr2FFQzteuHu9JsnSocX7tFnYPA labile@server";
 
