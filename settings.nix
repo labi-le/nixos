91f1f1aa782cb43ef5c738226ce4368f67f2b106
labi-le <1a6i1e@gmail.com>
2026-08-02 18:30:24 +0300

    settings: drop dead garnix cache, add nix-community and numtide

--- settings.nix
+++ settings.nix
@@ -15,15 +15,14 @@
       substituters = [
         # "https://cache.labile.cc?priority=-1"
         "https://cache.nixos.org?priority=10"
-        "https://cosmic.cachix.org?priority=20"
-        "https://cache.garnix.io?priority=100"
+        "https://nix-community.cachix.org?priority=20"
+        "https://cache.numtide.com?priority=30"
       ];
       trusted-public-keys = [
         # "cache.labile.cc:wsb7HUFrITCpBKIs+c4Uv3sau03Isb3CKL+5FrHZomw="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE="
-        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
       ];
       trusted-users = [ "@wheel" ];
       experimental-features = [
