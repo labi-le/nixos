354a877e88f96d27d679a1b11278c0cb04e2d2cd
labi-le <1a6i1e@gmail.com>
2026-09-09 17:17:48 +0300

    journald: move to settings.Journal

--- modules/journald.nix
+++ modules/journald.nix
@@ -1,6 +1,6 @@
 {
-  services.journald.extraConfig = ''
-    Storage=persistent
-    SystemMaxUse=1G
-  '';
+  services.journald.settings.Journal = {
+    Storage = "persistent";
+    SystemMaxUse = "1G";
+  };
 }
